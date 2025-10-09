045aef0dec9ce111bf1e61b93c172291be044db1
christian-branski-vinci <christian.branski@student.vinci.be>
2025-10-09 14:53:44 +0200

    new

---
+++ Web2/Exercice1.1/basic-ts-api-boilerplate/types.ts
@@ -0,0 +1,13 @@
+
+
+interface films{
+  id: number;
+  title: string;
+  director: string;
+  duration: number;
+  budget?: number;
+  description?: string;
+  imagerUrl?: string;
+}
+
+export type { films };
