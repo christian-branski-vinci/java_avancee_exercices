ff5e3460503f9b3b311689e43187cff1638d6653
christian-branski-vinci <christian.branski@student.vinci.be>
2025-10-22 09:20:52 +0200

    Ex 2.4 end

--- Web2/Exercices/Exercice2.4/vite-project/src/Components/App.tsx
+++ Web2/Exercices/Exercice2.4/vite-project/src/Components/App.tsx
@@ -1,5 +1,7 @@
-import Cinema from "../Cinema/Cinema";
-import PageTitle from "../PageTitle/PageTitle";
+import Cinema from "./Cinema";
+import PageTitle from "./PageTitle";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const App = () => {
   const pageTitle = "Informations sur les films dans les cinémas";
@@ -49,11 +51,21 @@ const App = () => {
 
   return (
     <div>
+      <Header urlLogo="https://plus.unsplash.com/premium_photo-1710366327112-1c6b77517ff4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1157">
+      <h1>Tout les films</h1>
+      </Header>
+      <main>
       <PageTitle title={pageTitle} />
 
       <Cinema name={cinema1Name} movies={moviesCinema1} />
 
       <Cinema name={cinema2Name} movies={moviesCinema2} />
+      
+      </main>
+
+      <Footer urlLogo="https://plus.unsplash.com/premium_photo-1710366327112-1c6b77517ff4?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1157">
+      <h1>c'était tout les films</h1>
+      </Footer>
     </div>
   );
 };
