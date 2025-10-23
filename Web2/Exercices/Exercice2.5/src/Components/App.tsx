30d267de1c22870dc1f3e448b753fd56e82607bb
christian-branski-vinci <christian.branski@student.vinci.be>
2025-10-23 14:53:52 +0200

    Ex2.5 end

--- Web2/Exercices/Exercice2.5/src/Components/App.tsx
+++ Web2/Exercices/Exercice2.5/src/Components/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ClickCounter from './ClickCounter'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '../../public/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -17,14 +16,10 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <ClickCounter 
+      title="Counter" 
+      on10clicks='You are a master at clicking'
+      hoverMessage='Click on me PLZ' />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ Web2/Exercices/Exercice2.5/src/Components/ClickCounter.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+
+interface ClickCounterProps {
+    title: string,
+    on10clicks: string
+    hoverMessage: string;
+}
+
+const ClickCounter = ({
+    title,
+    on10clicks,
+    hoverMessage }: ClickCounterProps) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [count, setCount] = useState(0);
+    const [isHovered, setIsHovered] = useState(false);
+    return (
+        <div className="card">
+            <h4>{title}</h4>
+            
+            {isHovered ? <p>{hoverMessage}</p>:null}
+            <button
+                onClick={() => setCount((count) => count + 1)}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={()=> setIsHovered(false)}
+                >
+                count is {count}
+            </button>
+            {count >= 10 ? <p>{on10clicks}</p> : null}
+            <p>
+                Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+        </div>
+    )
+}
+
+export default ClickCounter
